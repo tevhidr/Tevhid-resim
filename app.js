5396415708d3dadd8aafe0bf1a2a8a89407d1efd
Metin Mert <mmert6582@gmail.com>
2024-11-18 14:25:46 +0300

    statik

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-fn = () => {
-    console.log("asdasd");
-}
+const express = require('express')
+const path=require('path')
+const app =  express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/',(req,res)=> {
+   
+    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+})
+
+
+app.listen(port, () => {
+    console.log("Sunucu 3000 portunda başlatıldı")
+})
